e11a52d7677a67e8d1f4404e2efa670fc314ec55
unknown <tterekhovaa@mail.ru>
2024-04-21 13:58:32 +0300

    add remove/add admin methods

--- bot/handlers/admin/admin_handlers.py
+++ bot/handlers/admin/admin_handlers.py
@@ -8,30 +8,48 @@ from bot.core.Models import *
 
 admins_router = Router()
 
+
 @admins_router.callback_query(F.data.contains('show_admins'))
 async def show_admins(callback_query: types.CallbackQuery, bot: Bot, state: FSMContext):
-    msg = await bot.send_message(chat_id=callback_query.from_user.id, text='Получение списка администраторов...')
-    admins_list = repo.get_admins()
-    await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=msg.message_id,
-                                text='Список администраторов: ')
+    try:
+        msg = await bot.send_message(chat_id=callback_query.from_user.id, text='Получение списка администраторов...')
+        admins_list = repo.get_admins()
+        await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=msg.message_id,
+                                    text='Список администраторов: ')
 
-    for admin in admins_list:
-        sms = f"id: {admin.id}\n{admin.nickname}\n"
-        await bot.send_message(chat_id=callback_query.from_user.id, text=sms, reply_markup=remove_admin_kb(admin.id))
+        for admin in admins_list:
+            sms = f"id: {admin.id}\n{admin.nickname}\n"
+            await state.set_data({'sms': sms})
+            await bot.send_message(chat_id=callback_query.from_user.id, text=sms,
+                                   reply_markup=remove_admin_kb(admin.id))
 
-    add_text = 'Чтобы добавить администратора, нажмите на кнопку ниже'
-    await bot.send_message(chat_id=callback_query.from_user.id, text=add_text, reply_markup=add_admin_kb())
+        add_text = 'Чтобы добавить администратора, нажмите на кнопку ниже'
+        await bot.send_message(chat_id=callback_query.from_user.id, text=add_text, reply_markup=add_admin_kb())
+
+    except Exception as e:
+        await bot.send_message(chat_id=callback_query.from_user.id, text='Что-то пошло не так. Попробуйте снова.')
+        print(str(e))
 
 
 @admins_router.callback_query(F.data.contains('remove_admin'))
 async def remove_admin(callback_query: types.CallbackQuery, bot: Bot, state: FSMContext):
-    id = callback_query.data.split("_")[2]
-    msg = await bot.send_message(chat_id=callback_query.from_user.id, text='Удаление администратора...')
-    repo.remove_admin(id)
-    await bot.delete_message(chat_id=callback_query.from_user.id, message_id = msg.message_id-len(repo.get_admins()))
-    await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=msg.message_id, text='Администратор удален.')
-    # Удаляются все сообщения с именами админов
-    #+ кнопки
+    try:
+        id = callback_query.data.split("_")[2]
+        msg = await bot.send_message(chat_id=callback_query.from_user.id, text='Удаление администратора...')
+        response = repo.remove_admin(id)
+        if response['removed']:
+            for i in range(1, len(repo.get_admins()) + 4):
+                await bot.delete_message(chat_id=callback_query.from_user.id, message_id=msg.message_id - i)
+            await bot.edit_message_text(chat_id=callback_query.from_user.id, message_id=msg.message_id,
+                                        text='Администратор удален.', reply_markup=admins_start_keyboard())
+        else:
+            await bot.send_message(chat_id=callback_query.from_user.id,
+                                    text=f"Ошибка удаления администратора: {response['message']}")
+
+
+    except Exception as e:
+        await bot.send_message(chat_id=callback_query.from_user.id, text='Что-то пошло не так. Попробуйте снова.')
+        print(str(e))
 
 
 @admins_router.callback_query(F.data.contains('add_admin'))
@@ -55,10 +73,9 @@ async def add_admin(message: Message, bot: Bot, state: FSMContext):
     id = id['id']
     name = message.text
     response = repo.add_admin(AdminModel(id=id, nickname=name))
-    # 3 главные кнопки
-    #
+
     if response['added']:
-        await bot.send_message(chat_id=message.from_user.id, text='Администратор успешно добавлены!')
+        await bot.send_message(chat_id=message.from_user.id, text='Администратор успешно добавлен!', reply_markup=admins_start_keyboard())
     else:
         await bot.send_message(chat_id=message.from_user.id,
                                text=f"Ошибка добавления администратора: {response['message']}")
